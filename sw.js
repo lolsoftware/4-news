77a321a05409a4b1c58c91a49d7532dbe1f8f3a8
lolsoftware <lolsoftware@users.noreply.github.com>
2026-04-18 07:58:07 +0000

    deploy: d09ceeeff52d008f7fec0d5068478a63baecbcb1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'news-v2';
+const CACHE_NAME = 'news-v3';
 const STATIC_ASSETS = ['/', '/index.html', '/style.css', '/app.js', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', event => {
